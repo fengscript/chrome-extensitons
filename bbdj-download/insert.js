23bb337e2cf3c8a659831f796cc93b482a09f62b
young <fengyanggang@hotmail.com>
2021-05-16 23:27:47 +0800

    add method 3

--- bbdj-download/insert.js
+++ bbdj-download/insert.js
@@ -1,12 +1,25 @@
-let timer;
-function check() {
-  const resource = document.getElementById('jp_audio_0').getAttribute('src');
+// Method 1
+// let timer;
+// function check() {
+//   const resource = document.getElementById('jp_audio_0').getAttribute('src');
 
-  const content = document.querySelector('#layui-layer1 .layui-layer-content');
+//   const content = document.querySelector('#layui-layer1 .layui-layer-content');
 
-  if (content && content.childElementCount > 0) {
-    content.innerHTML = '<a href=' + resource + '>Download</a>';
-  }
+//   if (content && content.childElementCount > 0) {
+//     content.innerHTML = '<a href=' + resource + '>Download</a>';
+//   }
+// }
+
+// timer = setInterval(check, 1000);
+
+// Method 3
+document.querySelector('.playcz .dl').removeAttribute('onclick');
+
+function addEvt () {
+  document.querySelector('.playcz .dl').setAttribute('href', window.playurl2)
 }
 
-timer = setInterval(check, 1000);
+document.querySelector('.playcz .dl').addEventListener('click', addEvt)
+
+
+
